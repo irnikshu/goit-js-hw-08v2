082209826772f101a8a90e666cba96a88095328a
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-17 19:30:37 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,53 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallaryEl = document.querySelector('.gallery');
+gallaryEl.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+function createGallery(galleryItems) {
+   
+    return galleryItems
+     
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>
+</div>`
+        })
+        .join('');
+   
+}
+gallaryEl.addEventListener('click', onItemsClick);
+
+function onItemsClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(event.target);
+
+//     const lightbox = new SimpleLightbox('.gallery a', {
+//         captionsData: 'alt',
+//         captionDelay: 250,
+//         overlayOpacity: 0.8,
+//         closeText: '×',
+//         scrollZoom: false,
+
+// });
+}
+  const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        overlayOpacity: 0.8,
+        closeText: '×',
+        scrollZoom: false,
+
+});
