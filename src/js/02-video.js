83f7d5c4afb266fbe7b3020bd79f385caaa32dc9
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-18 01:15:37 +0200

    Update task 2v2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,29 @@
 import throttle from "lodash.throttle";
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
-const LOCALSTORAGE_KEY = "videoplayer-current-time";
+// const LOCALSTORAGE_KEY = "videoplayer-current-time";
 const player = new Player(iframe);
 
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({seconds}) {
-  try {
-    return localStorage.setItem(LOCALSTORAGE_KEY, seconds);
-  } catch (error) { 
-    console.log(error.name)
-    return [];
-  }
+// function onPlay({seconds}) {
+//   try {
+//     return localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+//   } catch (error) { 
+//     console.log(error.name)
+//     return [];
+//   }
   
-};
+// };
 
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+// player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+function onPlay(event) {
+  console.log(event.seconds)
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+
+const getContentOfLocalStorage = (
+  localStorage.getItem('videoplayer-current-time') || 0
+);
+player.setCurrentTime(getContentOfLocalStorage);
