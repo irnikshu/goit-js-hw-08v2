8a80640c6b6fdd0bebebcbb8f1e66bc564ddd735
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-18 00:20:52 +0200

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const player = new Player(iframe);
 
+load();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  console.log(data);
+  try {
+    return localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+  } catch (error) { 
+    console.log(error.name)
+    return [];
+  }
+  
+};
+
+function getTime() {
+  try {
+    const saveTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    return saveTime || [];
+  } catch (error) {
+    console.log(error.name)
+  }
+
+
+};
+
+function load() {
+create = getTime();
+
+player.setCurrentTime(create.seconds || 0);
+};
